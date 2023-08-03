8ff2656e35c012ac9ac1361e866ba0a074ece984
Rovshan13 <allakhverdievrovshann@gmail.com>
2023-08-03 15:48:31 +0400

    feat: ready footer and side panel

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import './footer.scss'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>Admin Panel</span>
+      <span>&copy; Rovshan Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
